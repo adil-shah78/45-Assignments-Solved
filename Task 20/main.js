48da516519ed93ae5b76f24ea2013dfc2a2d1933
adil-shah78 <165047493+adil-shah78@users.noreply.github.com>
2024-03-28 19:42:40 +0500

    Add files via upload

---
+++ Task 20/main.js
@@ -0,0 +1,6 @@
+// Making a programing languages Array
+var ProgramingLanguage = ["Typescript", "Javascripy", "python", "PHP"];
+// Print the massage of List
+console.log("List of Programing Language:");
+// Print the values of array in the form of List
+ProgramingLanguage.forEach(function (language) { return console.log(language); });
---
+++ Task 20/main.ts
@@ -0,0 +1,8 @@
+// Making a programing languages Array
+let ProgrammingLanguages : string[] = ["Typescript","Javascripy","python","PHP"];
+
+// Print the massage of List
+console.log("List of Programming Languages:");
+
+// Print the values of array in the form of List
+ProgrammingLanguages.forEach(language => console.log(language));
