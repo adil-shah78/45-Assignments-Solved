ccd665b411d4404aded305b5adbb2e7f78d47624
adil-shah78 <165047493+adil-shah78@users.noreply.github.com>
2024-03-28 19:42:19 +0500

    Add files via upload

---
+++ Task 19/main.js
@@ -0,0 +1,3 @@
+var guestList = ["adil", "gohar", "shan", "waseem", "nadeem"];
+var lengthGuest = guestList.length;
+console.log("we are inviting total ".concat(lengthGuest, " guests."));
---
+++ Task 19/main.ts
@@ -0,0 +1,5 @@
+let guestList = ["adil","gohar","shan","waseem","nadeem"];
+
+let lengthGuest: number = guestList.length;
+
+console.log(`we are inviting total ${lengthGuest} guests.`);
