91c495324a9f4968b1777fe46b6923b39eec736d
adil-shah78 <165047493+adil-shah78@users.noreply.github.com>
2024-03-28 19:43:02 +0500

    Add files via upload

---
+++ Task 21/main.js
@@ -0,0 +1,6 @@
+var itcourse = {
+    CourseName: "Typescript and Javascript",
+    Location: "Governor House Sindh",
+    OnSiteStudents: 50000
+};
+console.log(itcourse);
---
+++ Task 21/main.ts
@@ -0,0 +1,13 @@
+interface itcourse {
+    CourseName:string;
+    Location: string;
+    OnSiteStudents: number;
+}
+
+let itcourse = {
+    CourseName : "Typescript and Javascript",
+    Location : "Governor House Sindh",
+    OnSiteStudents : 50000
+};
+
+console.log(itcourse)
